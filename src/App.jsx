0b0030c8c7d4c6133cbb63b3753b5f9cd9b2933b
iberidze38 <iberidze38@gmail.com>
2023-07-13 17:52:17 +0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import GlobalStyles from './components/GlobalStyles'
 import Header from './components/Header'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter} from "react-router-dom"
 import Section from './components/Section'
 import React from 'react'
 
