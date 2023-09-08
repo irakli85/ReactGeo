fc15f02d7d78b48ddba852f319320ec3907d71e3
iberidze38 <iberidze38@gmail.com>
2023-09-08 16:45:03 +0400

    pure

--- src/components/Pure.jsx
+++ src/components/Pure.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { H1styled, H2styled, Pstyled1, ImgDiv1, Listyled1} from './HelloWorld'
+import data from '../data'
+import { DivSt } from './FirstComponent'
+import Learn from './Learn'
+import Nextpage from './Nextpage'
+import Pitfall from './Pitfall'
+import DeepDive from './DeepDive'
+import Note from './Note'
+
+import img103 from '../assets/103.png'
+import img104 from '../assets/104.png'
+import img105 from '../assets/105.png'
+import img106 from '../assets/106.png'
+import img107 from '../assets/107.png'
+import img108 from '../assets/108.png'
+import img109 from '../assets/109.png'
+import recipe from '../assets/recipe.png'
 
 function Pure() {
   return (
-    <div>Lorem2000</div>
+    <DivSt>
+      
+    </DivSt>
   )
 }
 
